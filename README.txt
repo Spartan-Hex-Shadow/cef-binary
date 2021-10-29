4699694fac4030b5ed76b4e33179d82e10464a4f
GeorgeVovos <george.vovos@prometric.com>
2021-10-29 16:26:32 +0100

    Added BTL. prefix to files and references

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
-The CEF Readme.txt file is now included as part of the cef.redist.x86 and cef.redist.x64 packages.
+The CEF Readme.txt file is now included as part of the BTL.cef.redist.x86 and cef.redist.x64 packages.
 
-It is located in the `CEF` folder contained within the `Nuget Package`. An example of the file location is `packages\cef.redist.x86.3.3626.1891\CEF\Readme.txt`
+It is located in the `CEF` folder contained within the `Nuget Package`. An example of the file location is `packages\BTL.cef.redist.x86.3.3626.1891\CEF\Readme.txt`
--- build.ps1
+++ build.ps1
@@ -494,11 +494,11 @@ try
 		}
 
 		# Build 32bit packages
-		. $Nuget pack nuget\cef.redist.nuspec -NoPackageAnalysis -Version $CefPackageVersion -Properties 'Configuration=Release;Platform=x86;CPlatform=windows32;' -OutputDirectory nuget
+		. $Nuget pack nuget\BTL.cef.redist.nuspec -NoPackageAnalysis -Version $CefPackageVersion -Properties 'Configuration=Release;Platform=x86;CPlatform=windows32;' -OutputDirectory nuget
 		. $Nuget pack nuget\chromiumembeddedframework.runtime.win.nuspec -NoPackageAnalysis -Version $CefPackageVersion -Properties 'Configuration=Release;Platform=x86;CPlatform=windows32;' -OutputDirectory nuget
 
 		# Build 64bit packages
-		. $Nuget pack nuget\cef.redist.nuspec -NoPackageAnalysis -Version $CefPackageVersion -Properties 'Configuration=Release;Platform=x64;CPlatform=windows64;' -OutputDirectory nuget
+		. $Nuget pack nuget\BTL.cef.redist.nuspec -NoPackageAnalysis -Version $CefPackageVersion -Properties 'Configuration=Release;Platform=x64;CPlatform=windows64;' -OutputDirectory nuget
 		. $Nuget pack nuget\chromiumembeddedframework.runtime.win.nuspec -NoPackageAnalysis -Version $CefPackageVersion -Properties 'Configuration=Release;Platform=x64;CPlatform=windows64;' -OutputDirectory nuget
 		
 		# Build arm64 packages
@@ -508,21 +508,21 @@ try
 		. $Nuget pack nuget\chromiumembeddedframework.runtime.nuspec -NoPackageAnalysis -Version $CefPackageVersion -Properties 'Configuration=Release;' -OutputDirectory nuget
 
 		# Build sdk
-		$Filename = Resolve-Path ".\nuget\cef.sdk.props"
-		$Text = (Get-Content $Filename) -replace '<CefSdkVer>.*<\/CefSdkVer>', "<CefSdkVer>cef.sdk.$CefPackageVersion</CefSdkVer>"
+		$Filename = Resolve-Path ".\nuget\BTL.cef.sdk.props"
+		$Text = (Get-Content $Filename) -replace '<CefSdkVer>.*<\/CefSdkVer>', "<CefSdkVer>BTL.cef.sdk.$CefPackageVersion</CefSdkVer>"
 		[System.IO.File]::WriteAllLines($Filename, $Text)
 
-		. $Nuget pack nuget\cef.sdk.nuspec -NoPackageAnalysis -Version $CefPackageVersion -OutputDirectory nuget
+		. $Nuget pack nuget\BTL.cef.sdk.nuspec -NoPackageAnalysis -Version $CefPackageVersion -OutputDirectory nuget
 	
 		if ($env:APPVEYOR_REPO_TAG -eq "True")
 		{
-			appveyor PushArtifact "nuget\cef.redist.x86.$CefPackageVersion.nupkg"
-			appveyor PushArtifact "nuget\cef.redist.x64.$CefPackageVersion.nupkg"
+			appveyor PushArtifact "nuget\BTL.cef.redist.x86.$CefPackageVersion.nupkg"
+			appveyor PushArtifact "nuget\BTL.cef.redist.x64.$CefPackageVersion.nupkg"
 			appveyor PushArtifact "nuget\chromiumembeddedframework.runtime.win-x86.$CefPackageVersion.nupkg"
 			appveyor PushArtifact "nuget\chromiumembeddedframework.runtime.win-x64.$CefPackageVersion.nupkg"
 			appveyor PushArtifact "nuget\chromiumembeddedframework.runtime.win-arm64.$CefPackageVersion.nupkg"
 			appveyor PushArtifact "nuget\chromiumembeddedframework.runtime.$CefPackageVersion.nupkg"
-			appveyor PushArtifact "nuget\cef.sdk.$CefPackageVersion.nupkg"
+			appveyor PushArtifact "nuget\BTL.cef.sdk.$CefPackageVersion.nupkg"
 		}
 	}
 
